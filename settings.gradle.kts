1f267232939395c9428d469f163ce9fb38abcb41
SpaceWolf <spacewolf_500@outlook.com>
2023-10-01 17:51:16 -0600

    PROYECTO 2 AD 7S21

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "p2_7S21"
+include(":app")
+ 
